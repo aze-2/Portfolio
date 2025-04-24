f135b489574e3cd27eb26c33c89c5efe71e9ba46
azetsu <ten.nob.r6@gmail.com>
2025-04-25 06:39:00 +0900

    エラー対応②漏れ

--- src/components/ui/color-mode.tsx
+++ src/components/ui/color-mode.tsx
@@ -45,7 +45,9 @@ export function ColorModeIcon() {
   return colorMode === "dark" ? <LuMoon /> : <LuSun />
 }
 
-interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
+// interface ColorModeButtonProps extends Omit<IconButtonProps, "aria-label"> {}
+type ColorModeButtonProps = Omit<IconButtonProps, "aria-label"> 
+
 
 export const ColorModeButton = React.forwardRef<
   HTMLButtonElement,
