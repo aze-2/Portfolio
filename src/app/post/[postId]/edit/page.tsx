d14f5d6d94744b199533b9472173ce01cc5854bf
azetsu <ten.nob.r6@gmail.com>
2025-04-26 22:42:01 +0900

    PostEditPage async修正

--- src/app/post/[postId]/edit/page.tsx
+++ src/app/post/[postId]/edit/page.tsx
@@ -3,22 +3,24 @@ import { createClient } from '../../../../../utils/supabase/server'
 import { notFound } from 'next/navigation'
 import PostEdit from '@/app/components/Post/Post-Edit'
 
-type PageProps = {
+type Props = {
     params: {
         postId: string
     }
 }
 
-const PostEditPage = async ({ params }: PageProps) => {
+export default async function PostEditPage({ params }: Props) {
     const supabase = await createClient()
 
         //ブログ詳細取得
-        const { data: post, error } = await supabase.from('posts').select().eq('id', params.postId).single()
-        console.log(post)
-        //ブログが存在しない場合
+        const { data: post, error } = await supabase
+         .from('posts')
+         .select()
+         .eq('id', params.postId)
+         .single()
+
+         //ブログが存在しない場合
         if (error || !post) return notFound()
 
   return <PostEdit post={post} />
 }
-
-export default PostEditPage
