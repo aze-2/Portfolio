f5f11d6a5ef0cfc612d8bf56697fb5e7d46c0a64
azetsu <ten.nob.r6@gmail.com>
2025-04-27 00:07:29 +0900

    props修正（Next.js15対応）

--- src/app/post/[postId]/edit/page.tsx
+++ src/app/post/[postId]/edit/page.tsx
@@ -4,19 +4,20 @@ import { notFound } from 'next/navigation'
 import PostEdit from '@/app/components/Post/Post-Edit'
 
 // type Props = {
-//     params: {
+//     params: Promise<{
 //         postId: string
-//     }
+//     }>;
 // }
 
-export default async function PostEditPage({ params }: { params: { postId: string } }) {
+export default async function PostEditPage(props: { params : Promise<{ postId: string }> }) {
     const supabase = await createClient()
+    const { postId } = await props.params;
 
         //ブログ詳細取得
         const { data: post, error } = await supabase
          .from('posts')
          .select()
-         .eq('id', params.postId)
+         .eq('id', postId)
          .single()
 
          //ブログが存在しない場合
